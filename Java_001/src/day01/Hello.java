bf3d3780bb233e7fee323855dd7f43bfc9daf87b
Ewinone <tmddnjs1122@naver.com>
2022-08-04 20:27:39 +0900

    Day01
    1일차
    - eclipse 사용하여 Hello World출력
    - 변수 선언 방법

---
+++ Java_001/src/day01/Hello.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class Hello {
+	// Class name rule
+	// 1. 첫 글자는 대문자
+	// 2. 첫 글자는 숫자 불가.
+	
+	
+	// Ctrl + / : 블록 범위 한줄 주석
+	// Ctrl + shift + /: 블록 범위 여러줄 주석
+	// 코드 줄 변경법: alt + 화살표
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");	//	콘솔창에 Hello World! 출력.		
+		//System.out.println : syso + ctrl + spacebar
+	}
+
+}
